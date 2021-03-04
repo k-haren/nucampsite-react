3de9d56921c305325eac370e43f8a0c86589f664
kharen <kharen@MacBook-Pro-13.local>
2021-03-03 17:57:58 -0800

    Components Part 2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'typeface-lobster';
+import 'typeface-open-sans';
+//Make sure that the import for App is positioned as the last one in the list of imports:
+import App from './App';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -15,3 +19,5 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
